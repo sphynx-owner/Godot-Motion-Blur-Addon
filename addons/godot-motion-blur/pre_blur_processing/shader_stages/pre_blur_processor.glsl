38c8d752f2076fa59c7cac68d2a450da64b7f8a8
sphynx-owner <lavisg2001@gmail.com>
2026-09-05 21:29:12 +0300

    make velocity thresholds independent of aspect ratio

--- addons/godot-motion-blur/pre_blur_processing/shader_stages/pre_blur_processor.glsl
+++ addons/godot-motion-blur/pre_blur_processing/shader_stages/pre_blur_processor.glsl
@@ -268,12 +268,14 @@ void main() {
 	}
 	// ---------------------------------------------------
 	
-	// Here is where we apply the velocity thresholds and the intensity, customized by the user.
+	// Here is where we apply the velocity thresholds and the intensity, customized by the user. Note that we scale
+	// the velocity that's fed into the thresholds so to negate the effects on aspect ratio on the perceived length of the 
+	// velocity. 
 	// If the previous position is happening behind the camera's near clip plane, which can happen when the camera moves backwards at high speed,
 	// the w component of the projected vector would be negative, and the velocity vector would be flipped.
 	// This happens with Godot's native motion vectors as well. We can detect this and flip them back, avoiding
 	// crazy artifacts.
-	total_velocity.xy *= sharp_step(params.velocity_threshold_lower, params.velocity_threshold_upper, length(total_velocity.xy)) * render_size * (view_past_ndc_cache.w < 0 ? -1 : 1) * params.motion_blur_intensity;
+	total_velocity.xy *= sharp_step(params.velocity_threshold_lower, params.velocity_threshold_upper, length(total_velocity.xy * vec2(1, float(render_size.y) / float(render_size.x)))) * render_size * (view_past_ndc_cache.w < 0 ? -1 : 1) * params.motion_blur_intensity;
 
 	// Now we clamp the velocity magnitudes to the tile size.
 	// We multiply the tile size by 2 because we blur the velocity
@@ -291,6 +293,6 @@ void main() {
 #ifdef DEBUG
 	imageStore(debug_2_image, uvi, vec4(texelFetch(depth_sampler, uvi, 0).x * 5));
 	imageStore(debug_3_image, uvi, vec4(-sampled_velocity, 0.0, 0.0));
-	imageStore(debug_4_image, uvi, vec4(-total_velocity.xy / render_size, abs(-total_velocity.z) * 1000, depth));
+	imageStore(debug_4_image, uvi, vec4(abs(total_velocity.xy) / render_size, abs(-total_velocity.z) * 1000, depth));
 #endif
 }
